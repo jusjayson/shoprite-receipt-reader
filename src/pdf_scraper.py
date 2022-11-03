8e67d41a8fdd6aa69300af8784855205edf52483
Jayson Dorsett <ja_dorsett@jdorsett.com>
2022-11-03 18:01:58 -0400

    chore: remove prefix text from items

--- src/pdf_scraper.py
+++ src/pdf_scraper.py
@@ -1,4 +1,16 @@
 from PyPDF2 import PdfReader
+import re
+
+
+def add_osn_to_word(word):
+    """
+    Handle unexpected pattern violations caused by spaces and newlines
+
+    - PyPDF2 may inject unexpected spaces when reading PDF
+    """
+    osn = r"(\s\n)?"  # optional space, newline
+
+    return rf"{osn}" + rf"{osn}".join(rf"{letter}" for letter in word)
 
 
 def get_raw_items_from_pdf(path):
@@ -12,6 +24,9 @@ def get_raw_items_from_pdf(path):
         full_txt += page.extract_text()
 
     full_txt = full_txt.replace("\xa0", "")
+    full_txt = full_txt[
+        re.search(rf"{add_osn_to_word('Groceries')}:", full_txt).end() :
+    ]  # Start after groceries label
 
-    print(full_txt)
+    print([full_txt])
     return []
