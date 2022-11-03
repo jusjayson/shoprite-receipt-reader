691a09d975f3ad773bfbe0895d529b379e33c285
Jayson Dorsett <ja_dorsett@jdorsett.com>
2022-11-03 18:20:54 -0400

    feat: get raw item from pdf

--- src/pdf_scraper.py
+++ src/pdf_scraper.py
@@ -8,7 +8,7 @@ def add_osn_to_word(word):
 
     - PyPDF2 may inject unexpected spaces when reading PDF
     """
-    osn = r"(\s\n)?"  # optional space, newline
+    osn = r"[\s\n]?"  # optional space, newline
 
     return rf"{osn}" + rf"{osn}".join(rf"{letter}" for letter in word)
 
@@ -23,8 +23,9 @@ def get_raw_items_from_pdf(path):
     for page in reader.pages:
         full_txt += page.extract_text()
 
-    # Remove unnecessary chars
+    # Remove/Replace unnecessary chars
     full_txt = full_txt.replace("\xa0", "")
+    full_txt = full_txt.replace("×", "x")
 
     # Remove prefix text
     full_txt = full_txt[
@@ -36,5 +37,14 @@ def get_raw_items_from_pdf(path):
         : re.search(add_osn_to_word("Payment Information"), full_txt).start()
     ]
 
+    matches = re.findall(
+        rf"([\w\-_\s\d\']+\n)"  # name
+        + rf"([\d\s]+\n?)"  # sku
+        + rf"(\d+\s+x\s+\$\d+\.\d+)"  # unit at price
+        + rf"({add_osn_to_word('Price:')}\s*?\$\d+\.\d+/\w+\n?|\$\d+\.\d+\n)?"  # price per unit
+        + rf".*?"
+        + rf"({add_osn_to_word('Qty:')}\s*\d+\.?\d*?\w+)?",  # quantity of unit
+        full_txt,
+    )
     print([full_txt])
-    return []
+    return matches
--- src/tests/test_scrape_pdf.py
+++ src/tests/test_scrape_pdf.py
@@ -8,15 +8,15 @@ def remove_spaces_and_lines(word):
 
 
 def test_get_single_raw_item():
-    raw_items = get_raw_items_from_pdf(f"{PDF_DIR}/shoprite_edited_one_item.pdf")
+    raw_item = get_raw_items_from_pdf(f"{PDF_DIR}/shoprite_edited_one_item.pdf")[0]
     assert remove_spaces_and_lines("BROC CRWNS RPC") in remove_spaces_and_lines(
-        raw_items[0]
+        raw_item[0]
     )
-    assert remove_spaces_and_lines("3082") in remove_spaces_and_lines(raw_items[1])
-    assert remove_spaces_and_lines("1 x $6.41") in remove_spaces_and_lines(raw_items[2])
+    assert remove_spaces_and_lines("3082") in remove_spaces_and_lines(raw_item[1])
+    assert remove_spaces_and_lines("1 x $6.41") in remove_spaces_and_lines(raw_item[2])
     assert remove_spaces_and_lines("Price: $1.99/lb") in remove_spaces_and_lines(
-        raw_items[3]
+        raw_item[3]
     )
     assert remove_spaces_and_lines("Qty: 3.22lb") in remove_spaces_and_lines(
-        raw_items[4]
+        raw_item[4]
     )
