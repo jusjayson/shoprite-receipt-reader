2d5865c2077e9744cb2e7eed34160252d155ba83
Jayson Dorsett <ja_dorsett@jdorsett.com>
2022-11-04 15:03:16 -0400

    test: coupon code

--- src/tests/test_scrape_pdf.py
+++ src/tests/test_scrape_pdf.py
@@ -46,10 +46,10 @@ def test_get_single_raw_item():
     assert remove_spaces_and_lines("3082") in remove_spaces_and_lines(raw_item[1])
     assert remove_spaces_and_lines("1 x $6.41") in remove_spaces_and_lines(raw_item[2])
     assert remove_spaces_and_lines("Price: $1.99/lb") in remove_spaces_and_lines(
-        raw_item[3]
+        raw_item[4]
     )
     assert remove_spaces_and_lines("Qty: 3.22lb") in remove_spaces_and_lines(
-        raw_item[4]
+        raw_item[5]
     )
 
 
@@ -71,8 +71,9 @@ def test_get_raw_items_on_single_page():
         raw_item_two[2]
     )
     assert remove_spaces_and_lines("Price: $6.00/lb") in remove_spaces_and_lines(
-        raw_item_two[3]
+        raw_item_two[4]
     )
+
 
 def test_get_one_item_per_page():
     raw_items = get_raw_items_from_pdf(f"{PDF_DIR}/shoprite_edited_one_item_per_pg.pdf")
@@ -102,6 +103,13 @@ def test_get_one_item_per_page():
     )
 
 
+def test_coupon_code():
+    raw_item = get_raw_items_from_pdf(f"{PDF_DIR}/shoprite_edited.pdf")[2]
+    assert remove_spaces_and_lines(
+        "**SC**Green Peppers: $0.72"
+    ) in remove_spaces_and_lines(raw_item[3])
+
+
 def test_across_page():
     raw_items = get_raw_items_from_pdf(
         f"{PDF_DIR}/shoprite_edited_item_across_page.pdf"
@@ -117,8 +125,8 @@ def test_across_page():
     assert remove_spaces_and_lines("3125") in remove_spaces_and_lines(raw_item[1])
     assert remove_spaces_and_lines("1 x $0.30") in remove_spaces_and_lines(raw_item[2])
     assert remove_spaces_and_lines("Price: $6.00/lb") in remove_spaces_and_lines(
-        raw_item[3]
+        raw_item[4]
     )
     assert remove_spaces_and_lines("Qty: 0.05lb") in remove_spaces_and_lines(
-        raw_item[4]
+        raw_item[5]
     )
