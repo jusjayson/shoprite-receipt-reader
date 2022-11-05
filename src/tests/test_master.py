02cf24782037764b7016157d52dd336e53367def
Jayson Dorsett <ja_dorsett@jdorsett.com>
2022-11-04 20:44:49 -0400

    test: replace name and cat with data from master during parse

--- src/tests/test_master.py
+++ src/tests/test_master.py
@@ -8,3 +8,11 @@ def test_import_master_info():
     assert master_info["1"]["Name"] == "Patayters"
     assert master_info["1"]["Cat"] == "Food"
     assert master_info["3"]["Name"] == "Iron"
+
+
+def test_master_replaces_name_and_cat():
+    parsed_item = get_parsed_items_from_pdf(
+        f"{PDF_DIR}/shoprite_edited.pdf", f"{PDF_DIR}/master.csv"
+    )[0]
+    assert parsed_item["Name"] == "Fresh Brocolli"
+    assert parsed_item["Cat"] == "Food"
